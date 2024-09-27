e2ee5272b7f77b1b7a183474be663d0b24a8a791
Gino <ginoalodar@gmail.com>
2024-09-27 05:45:40 -0500

    fix: returned cors allow all origins to false

--- cv_parser/cv_parser/settings.py
+++ cv_parser/cv_parser/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "https://ats-evaluator-frontend.vercel.app",
 ]
